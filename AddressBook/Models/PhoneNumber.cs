2cccfffe0218a515282b7cd28c7b0e57bbea7979
Zackery Mowrer <Zackmowrer@gmail.com>
2017-10-01 17:28:44 -0400

    Wrote out dummy PhoneNumber class

--- AddressBook/Models/PhoneNumber.cs
+++ AddressBook/Models/PhoneNumber.cs
@@ -1,9 +1,42 @@
-﻿namespace AddressBook.Models
+﻿using AddressBook.Models.Enums;
+
+namespace AddressBook.Models
 {
 	/// <summary>
 	/// Class representing a contact's phone number field.
 	/// </summary>
 	public class PhoneNumber
 	{
+		// Properties
+		public PhoneNumberType TypeOfPhoneNumber { get; protected set; }
+		public string Number { get; protected set; }
+
+		// Constructor
+		public PhoneNumber(PhoneNumberType typeOfPhoneNumber, string number)
+		{
+			TypeOfPhoneNumber = typeOfPhoneNumber;
+			Number = number;
+		}
+
+		//public static PhoneNumber Create()
+		//{
+
+		//}
+
+		// Overrides
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+
+		public override bool Equals(object obj)
+		{
+			return base.Equals(obj);
+		}
 	}
 }
