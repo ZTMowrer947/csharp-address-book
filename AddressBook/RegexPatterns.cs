a09d538f3b671f0022be0eedf27450e4fe2d2da1
Zackery Mowrer <Zackmowrer@gmail.com>
2017-10-01 16:34:26 -0400

    Added static class for regex patterns

---
+++ AddressBook/RegexPatterns.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace AddressBook
+{
+	public static class RegexPatterns
+	{
+		public static Dictionary<string, Regex> Address = new Dictionary<string, Regex>
+		{
+			{"Street Address", new Regex(@"^(\d{3,4} )?([A-z0-9]+ )+[A-z]+$")},
+			{"City", new Regex(@"^([A-z]+ )?[A-z]+$")},
+			{"State", new Regex(@"^[A-Z]{2}$")},
+			{"Postal Code", new Regex(@"^\d{5}(-\d{4})?$")}
+		};
+	}
+}
