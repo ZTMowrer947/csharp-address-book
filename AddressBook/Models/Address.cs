575c05548068a182ca1edb850dd14613b1fd95e3
Zackery Mowrer <Zackmowrer@gmail.com>
2017-10-01 16:47:17 -0400

    Wrote out Address class to pass its tests

--- AddressBook/Models/Address.cs
+++ AddressBook/Models/Address.cs
@@ -29,17 +29,44 @@
 		// Overrides
 		public override string ToString()
 		{
-			return base.ToString();
+			string formatString = string.Format("{0}: {1}, {2}, {3} {4}",
+				Type,
+				StreetAddress,
+				City,
+				State,
+				PostalCode
+			);
+
+			return formatString;
 		}
 
 		public override int GetHashCode()
 		{
-			return base.GetHashCode();
+			int hash = 0;
+			if (Type != null) hash += Type.GetHashCode();
+			if (StreetAddress != null) hash += StreetAddress.GetHashCode();
+			if (City != null) hash += City.GetHashCode();
+			if (State != null) hash += State.GetHashCode();
+			if (PostalCode != null) hash += PostalCode.GetHashCode();
+
+			return hash;
 		}
 
 		public override bool Equals(object obj)
 		{
-			return base.Equals(obj);
+			if (obj == null || GetType() != obj.GetType()) {
+				return false;
+			} else {
+				Address that = (Address)obj;
+
+				return (
+					Type == that.Type &&
+					StreetAddress == that.StreetAddress &&
+					City == that.City &&
+					State == that.State &&
+					PostalCode == that.PostalCode
+				);
+			}
 		}
 	}
 }
