28ed321d1644c7c94989e068baa16d365d03acc3
Zackery Mowrer <Zackmowrer@gmail.com>
2017-10-01 21:31:17 -0400

    Created tests for EmailAddress class

---
+++ AddressBookTests/Models/EmailAddressTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.Mail;
+using NUnit.Framework;
+using AddressBook.Models;
+using AddressBook.Models.Enums;
+
+namespace AddressBook.Tests.Models
+{
+	[TestFixture()]
+	public class EmailAddressTests
+	{
+		public EmailAddress emailAddress;
+		public EmailAddress sameEmailAddress;
+		public EmailAddress differentEmailAddress;
+
+		[OneTimeSetUp()]
+		public void SetUp()
+		{
+			EmailAddressType type = EmailAddressType.Home;
+			EmailAddressType differentType = EmailAddressType.Work;
+
+			string address = "me@example.com";
+
+			emailAddress = new EmailAddress(type, address);
+			sameEmailAddress = new EmailAddress(type, address);
+			differentEmailAddress = new EmailAddress(differentType, address);
+		}
+
+		[Test()]
+		public void EmailValidityTest()
+		{
+			TestDelegate testDel = () =>
+			{
+				MailAddress mailAddress = new MailAddress(emailAddress.Address);
+			};
+
+			Assert.DoesNotThrow(testDel);
+		}
+
+		[Test()]
+		public void StringConversionTest()
+		{
+			string expected = "Home: me@example.com";
+			string actual = emailAddress.ToString();
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test()]
+		public void EqualityTest()
+		{
+			Assert.AreEqual(emailAddress, sameEmailAddress);
+			Assert.AreNotSame(emailAddress, sameEmailAddress);
+			Assert.AreNotEqual(emailAddress, differentEmailAddress);
+		}
+	}
+}
