a67c3520f7fb95ee147b0f900bb95821b6dbb576
Zackery Mowrer <Zackmowrer@gmail.com>
2017-10-01 21:55:37 -0400

    Wrote out dummy Contact class

--- AddressBook/Models/Contact.cs
+++ AddressBook/Models/Contact.cs
@@ -1,9 +1,66 @@
-﻿namespace AddressBook.Models
+﻿using System.Collections.Generic;
+
+namespace AddressBook.Models
 {
 	/// <summary>
 	/// Class representing a contact in an address book.
 	/// </summary>
 	public sealed class Contact
 	{
+		// Properties
+		private string firstName;
+		private string lastName;
+		
+		public string Name
+		{
+			get
+			{
+				return string.Format("{0} {1}", firstName, lastName);
+			}
+
+			private set
+			{
+				int spaceOffset = value.IndexOf(' ');
+
+				firstName = value.Substring(0, spaceOffset);
+				lastName = value.Substring(spaceOffset + 1);
+			}
+		}
+
+		public HashSet<Address> Addresses { get; }
+		public HashSet<PhoneNumber> PhoneNumbers { get; }
+		public HashSet<FaxNumber> FaxNumbers { get; }
+		public HashSet<EmailAddress> EmailAddresses { get; }
+
+		// Constructor
+		public Contact(string name)
+		{
+			Name = name;
+			Addresses = new HashSet<Address>();
+			PhoneNumbers = new HashSet<PhoneNumber>();
+			FaxNumbers = new HashSet<FaxNumber>();
+			EmailAddresses = new HashSet<EmailAddress>();
+		}
+
+		//public static Contact Create()
+		//{
+
+		//}
+
+		// Overrides
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+
+		public override bool Equals(object obj)
+		{
+			return base.Equals(obj);
+		}
 	}
 }
