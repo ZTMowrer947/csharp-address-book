a17875cc2958303b35621175d0a18ff107abff81
Zackery Mowrer <Zackmowrer@gmail.com>
2017-10-01 21:14:30 -0400

    Wrote out dummy EmailAddress class

--- AddressBook/Models/EmailAddress.cs
+++ AddressBook/Models/EmailAddress.cs
@@ -1,9 +1,42 @@
-﻿namespace AddressBook.Models
+﻿using AddressBook.Models.Enums;
+
+namespace AddressBook.Models
 {
 	/// <summary>
 	/// Class representing a contact's email address field.
 	/// </summary>
 	public sealed class EmailAddress
 	{
+		// Properties
+		public EmailAddressType TypeOfEmailAddress { get; private set; }
+		public string Address { get; private set; }
+
+		// Constructor
+		public EmailAddress(EmailAddressType typeOfEmailAddress, string address)
+		{
+			TypeOfEmailAddress = typeOfEmailAddress;
+			Address = address;
+		}
+
+		//public static EmailAddress Create()
+		//{
+
+		//}
+
+		// Overrides
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+
+		public override bool Equals(object obj)
+		{
+			return base.Equals(obj);
+		}
 	}
 }
