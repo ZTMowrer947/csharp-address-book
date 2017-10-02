a2fded08b945fdc54d4764edebbc35dd3f0d1758
Zackery Mowrer <Zackmowrer@gmail.com>
2017-10-02 05:46:41 -0400

    Added InvalidValidationException class

---
+++ AddressBook/Exceptions/InputValidationException.cs
@@ -0,0 +1,11 @@
+﻿namespace AddressBook.Exceptions
+{
+	/// <summary>
+	/// Input was invalid (did not match its regex)
+	/// </summary>
+	public class InputValidationException: AddressBookException
+	{
+		public InputValidationException(string message) : base(message) { }
+		public InputValidationException() : base() { }
+	}
+}
