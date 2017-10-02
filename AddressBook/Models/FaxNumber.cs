e4b6f1a105db71a4b788c29eef3fcc7a1ac690d9
Zackery Mowrer <Zackmowrer@gmail.com>
2017-10-01 21:08:16 -0400

    Wrote out FaxNumber class

--- AddressBook/Models/FaxNumber.cs
+++ AddressBook/Models/FaxNumber.cs
@@ -1,9 +1,37 @@
-﻿namespace AddressBook.Models
+﻿using AddressBook.Models.Enums;
+
+namespace AddressBook.Models
 {
 	/// <summary>
 	/// Class representing a contact's fax number field.
 	/// </summary>
-	//public sealed class FaxNumber: PhoneNumber
-	//{
-	//}
+	public sealed class FaxNumber : PhoneNumber
+	{
+		// Constructor
+		public FaxNumber(string number) : base(PhoneNumberType.Fax, number)
+		{
+			TypeOfPhoneNumber = PhoneNumberType.Fax;
+		}
+
+		//public new static FaxNumber Create()
+		//{
+
+		//}
+
+		// Override
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj == null || GetType() != obj.GetType()) {
+				return false;
+			} else {
+				FaxNumber that = (FaxNumber)obj;
+				return base.Equals(that);
+			}
+		}
+	}
 }
